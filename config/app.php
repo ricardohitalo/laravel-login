fc5a35a8cfc1341c55363e3f9e6a1d4b9c126536
Ricardo Hitalo Araujo Soares <ricardohitalo1@gmail.com>
2022-09-03 14:27:24 -0300

    add jwt

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
+
     ],
 
     /*
